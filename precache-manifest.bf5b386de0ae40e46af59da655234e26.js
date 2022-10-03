d2668f41375de2c95907aa3ae6a495b29dbd85dc
Сергей Фомин <xaoc615@gmail.com>
2022-10-04 00:19:49 +0300

    Updates

---
+++ precache-manifest.bf5b386de0ae40e46af59da655234e26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e2c9bb105a7f89827f8baeda38878a",
+    "url": "/HomeworkIgnat/index.html"
+  },
+  {
+    "revision": "6444db0223636184bd55",
+    "url": "/HomeworkIgnat/static/css/main.1c1373d0.chunk.css"
+  },
+  {
+    "revision": "331c90cfe2dacc61b6d3",
+    "url": "/HomeworkIgnat/static/js/2.57c6547c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/HomeworkIgnat/static/js/2.57c6547c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6444db0223636184bd55",
+    "url": "/HomeworkIgnat/static/js/main.d5df5f6a.chunk.js"
+  },
+  {
+    "revision": "716cc365e6f4c41da338",
+    "url": "/HomeworkIgnat/static/js/runtime-main.e25656a4.js"
+  }
+]);
