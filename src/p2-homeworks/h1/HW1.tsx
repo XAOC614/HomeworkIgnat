ae5e4bede577dd7c5d49ddb0ed4daaf0d981a776
Сергей Фомин <xaoc615@gmail.com>
2022-10-03 19:27:09 +0300

    first commit

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Message from "./Message";
 const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
+    avatarSrc:'https://avatarko.ru/img/kartinka/2/Gubka_Bob.jpg',
+    name: 'Bob',
+    message: 'npm start?',
     time: '22:00',
 }
 
@@ -15,12 +15,12 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatarSrc={messageData.avatarSrc}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import s from './Message.module.css'
+type MessageType={
+    avatarSrc:string
+    name:string
+    message:string
+    time:string
+}
 
-function Message() {
+function Message(props:MessageType) {
     return (
-        <div>
+        <article className={s.message}>
+           <div>
+               <img className={s.imageBlock} src={props.avatarSrc}/>
+           </div>
 
-        </div>
+           <div className={s.textBlock}>
+               <div className={s.name}>
+                   <h2 className={s.name}>{props.name}</h2>
+                   <p className={s.text}>{props.message}</p>
+               </div>
+
+               <div className={s.time}>
+                   {props.time}
+               </div>
+           </div>
+        </article>
     )
 }
 
